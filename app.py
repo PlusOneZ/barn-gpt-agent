466323a94bac3ae870aaac1947f96baa278ed62e
Zhengyi Zhuo <zhuozhengyi@icloud.com>
2024-04-18 23:10:07 +0800

    Dummpy body not used

--- app.py
+++ app.py
@@ -23,12 +23,10 @@ def hello_world():  # put application's code here
 @app.route('/api/task/dummy', methods=['POST'])
 def dummy_api():
     # get data and hook from request body
-    data = request.json['data']
     hook = request.json['hook']
-    print(data, hook)
 
     # dummy tasks that takes 5 seconds to complete
-    ret = task_manager.create("dummy", data, hook)
+    ret = task_manager.create("dummy", "dummy", hook)
     return "OK" if ret else ("Failed", 400)
 
 
